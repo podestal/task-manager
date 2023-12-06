d55ac3357b04b6612c6254cb16d9146824703a08
podestal <luisrodriguezpodesta@gmail.com>
2023-12-06 11:10:37 -0500

    auth and create note

--- notes/serializers.py
+++ notes/serializers.py
@@ -4,4 +4,9 @@ from . import models
 class NoteSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Note
-        fields = '__all__'
+        fields = ['id', 'title', 'content', 'created_at']
+
+    def save(self, **kwargs):
+        user_id = self.context['user_id']
+        self.instance = models.Note.objects.create(user_id = user_id, **self.validated_data)
+        return self.instance
--- notes/views.py
+++ notes/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 from . import models
 from . import serializers
 
 class NotesViewSet(ModelViewSet):
-    queryset = models.Note.objects.all()
+
     serializer_class = serializers.NoteSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user_id = self.request.user.id
+        return models.Note.objects.filter(user_id = user_id)
+    
+    def get_serializer_context(self):
+        return {'user_id' : self.request.user.id}
